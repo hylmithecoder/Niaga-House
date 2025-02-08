d53ea53d4f714128267e691bdbc731269cf0c4be
hylmithecoder <hylmimuhammadfiarymahdi@gmail.com>
2025-02-08 20:36:51 +0700

    Niaga House 1.0

--- src/Pages/AddProperty.jsx
+++ src/Pages/AddProperty.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://192.168.98.126:5000" /* && "http://localhost:5000"*/;
+const API_URL = process.env.REACT_APP_API_URL || "https://endpoint-niaga-production.up.railway.app" /* && "http://localhost:5000"*/;
 
 const AddProperty = () => { 
   console.log(`${API_URL}/properties`);
